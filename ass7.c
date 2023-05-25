cdf8caed4a1c8738f136dbefd82c15bc70940383
Vedant Sonawane <98942578+Vedant2s@users.noreply.github.com>
2023-05-25 04:07:10 -0700

    Update ass7.c

--- ass7.c
+++ ass7.c
@@ -3,30 +3,31 @@
 int main() {
     int a[5]={5,4,3,2,1},temp;
     for(int i=0;i<5;i++){
-        for(int j=i;j<5;j++){
-            if(a[i]>a[j])
+        for(int j=i;j<4;j++){
+            if(a[j]>a[j+1])
             {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             
             }
         }}
-        TRISB=0;
-        PORTB=a;
+//        TRISB=0;
+//        PORTB=a;
         int b[5]={1,2,3,4,5};
         for(int i=0;i<5;i++){
-        for(int j=i;j<5;j++){
-            if(b[i]>b[j])
+        for(int j=i;j<4;j++){
+            if(b[j]<b[j+1])
             {
-                temp=b[i];
-                b[i]=b[j];
-                b[j]=temp;
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             
             }
         }}
-        TRISC=0;
-        PORTC=b;
+//        TRISC=0;
+//        PORTC=b;
     
     return 0;
 }
+
